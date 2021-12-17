d595b5b6cca718250dc4eea0f0f82758b413857e
Rachit Lalla <42689981+RachitLalla@users.noreply.github.com>
2021-12-17 22:44:28 +0000

    Need to try out something 18-dec-2021 and ask a doubt

--- main.py
+++ main.py
@@ -177,3 +177,40 @@ nonlocalvaraccess()
 print("After nonlocalvaraccess():"+str(x))
 
 print("c: "+str(c)) ##will print orignal c=100 not the modified value in nonlocalvaraccess() function
+'''
+---------------------------------------------------------
+TRY OUT TOMORROW 18th dec 2021
+Also, Try to create a function to read lines of code, print them each of them with their corresponding output and line number 
+'''
+largenum=int(1000000000000000033)
+
+idOfTrue=id(True)
+idOfIdOfTrue=id(id(True))
+idOf1=id(1)
+idOfIdOf1=id(id(1))
+idOfTrueVar2 = (id(True))
+idOf1Var2 = (id(1))
+idOfIdOfTrueVar2 = id(id(True))
+idOfIdOf1Var2 = id(id(1))
+
+
+
+
+
+
+print ((id(True)), ((id(True))),(id(1)),id(1))
+print (id(id(True)), id(id(True)),id(id(1)),id(id(1)))
+
+print (id(id(True)), id(id(True)),id(id(1)),id(id(1)))
+
+print ((id(True)), ((id(True))),(id(1)),id(1))
+print (id(id(True)), id(id(True)),id(id(1)),id(id(1)))
+"""
+
+Output: 
+140230620037120 140230620037120 140230620329696
+140230555530032 140230555530032 140230555530032
+
+"""
+
+print ((id(1000000000000000033)), ((id(1000000000000000033))),(id(largenum)))
